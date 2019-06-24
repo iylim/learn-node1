92fb7bb13ea2601dfc75e9ff13b2d7046a8c54e4
Ivy <yunjilim@gmail.com>
2019-06-24 11:57:17 -0700

    module 2 completed

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,18 +1,9 @@
 const express = require('express');
-
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  const wes = { name: 'Wes', age: 100 };
-  res.render('hello', {
-    dog: 'Hugo'
-  });
-});
+router.get('/', storeController.myMiddleware, storeController.homePage);
 
-// router.get('/reverse/:name', (req, res) => {
-//   const reverse = [...req.params.name].reverse().join('');
-//   res.send(reverse);
-// });
 
 module.exports = router;
