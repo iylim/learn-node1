6c4a60ef16177ac068a2a7757f150699bd7422f9
Ivy <yunjilim@gmail.com>
2019-06-24 11:26:50 -0700

    first commit

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+
 const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const wes = { name: 'Wes', age: 100 };
+  res.render('hello', {
+    dog: 'Hugo'
+  });
 });
+
+// router.get('/reverse/:name', (req, res) => {
+//   const reverse = [...req.params.name].reverse().join('');
+//   res.send(reverse);
+// });
 
 module.exports = router;
